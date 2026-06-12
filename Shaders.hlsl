3bce712b8e9470ca4e69748d34cd7c3162fdc43a
ojasmahajan2 <ojasamahajan.85@gmail.com>
2026-06-12 23:53:18 +0530

    Add files via upload

---
+++ Shaders.hlsl
@@ -0,0 +1,38 @@
+struct VSInput {
+    float2 position : POSITION;
+    float3 color    : COLOR;
+};
+
+struct VSOutput {
+    float4 pos       : SV_Position;
+    float3 color     : COLOR;
+    float  pointSize : SV_PointSize;
+};
+
+struct FSInput {
+    float4 pos   : SV_Position;
+    float3 color : COLOR;
+};
+
+[[vk::push_constant]]
+cbuffer PushConstants {
+    float4x4 mvp;
+};
+
+[shader("vertex")]
+VSOutput vertexMain(VSInput input) {
+    VSOutput output;
+    
+    float4 localPosition = float4(input.position, 0.0, 1.0);
+    
+    output.pos = mul(mvp, localPosition);
+    output.color = input.color;
+    output.pointSize = 5.0;
+    
+    return output;
+}
+
+[shader("fragment")]
+float4 fragmentMain(FSInput input) : SV_Target {
+    return float4(input.color, 1.0);
+}
